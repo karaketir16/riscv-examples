11640a2f8c77afe4b1c3e08d48bc562fecfb379b
karaketir16 <osmankaraketir@gmail.com>
2024-10-14 21:11:38 +0300

    little modify, worked with rv64 and sifive-u54

--- example/privilege/entry.s
+++ example/privilege/entry.s
@@ -2,22 +2,25 @@
 .globl start
 
 start:
-    la      t0, supervisor         # Load address of 'supervisor' into t0
-    csrw    mepc, t0               # Write t0 to mepc
-    la      t1, m_trap             # Load address of 'm_trap' into t1
-    csrw    mtvec, t1              # Write t1 to mtvec
+    # Load address of 'supervisor' into t0 and set mepc
+    la      t0, supervisor
+    csrw    mepc, t0
+
+    # Load address of 'm_trap' into t1 and set mtvec
+    la      t1, m_trap
+    csrw    mtvec, t1
 
     # Set up PMP to allow S-mode and U-mode full access to memory
-    li      t5, 0x1f               # NAPOT + RWX
+    li      t5, 0x1F               # NAPOT + R/W/X permissions
     csrw    pmpcfg0, t5            # Write to pmpcfg0
     li      t6, -1                 # All ones to cover entire address space
     csrw    pmpaddr0, t6           # Write to pmpaddr0
 
     # Correctly set MPP to Supervisor Mode
     csrr    t2, mstatus            # Read mstatus into t2
-    li      t3, ~(0x3 << 11)       # Create mask to clear MPP bits
+    li      t3, ~(0x3 << 11)       # Mask to clear MPP bits
     and     t2, t2, t3             # Clear MPP bits
-    li      t4, 0x1 << 11          # Set MPP to Supervisor Mode (0x1)
+    li      t4, (0x1 << 11)        # MPP = 01 (Supervisor Mode)
     or      t2, t2, t4             # Set MPP bits to 01
     csrw    mstatus, t2            # Write back to mstatus
 
@@ -25,32 +28,35 @@ start:
     li      t5, 0x100              # Exception code 8 (bit 8)
     csrs    medeleg, t5            # Delegate exception to S-mode
 
-    mret                            # Return from machine mode to mepc
-
+    mret                            # Return from M-mode to address in mepc
 
 m_trap:
-    csrr    t0, mepc
-    csrr    t1, mcause
-    la      t2, supervisor
-    csrw    mepc, t2
-    mret
+    csrr    t0, mepc               # Read mepc into t0
+    csrr    t1, mcause             # Read mcause into t1
+    la      t2, supervisor         # Load address of 'supervisor' into t2
+    csrw    mepc, t2               # Write t2 to mepc
+    mret                            # Return from M-mode to mepc
 
 supervisor:
     # Disable virtual memory by setting satp to zero
     csrw    satp, x0               # Write zero to satp
 
-    la      t0, user               # Load address of 'user' into t0
-    csrw    sepc, t0               # Write t0 to sepc
-    la      t1, s_trap             # Load address of 's_trap' into t1
-    csrw    stvec, t1              # Write t1 to stvec
-    sret                            # Return from supervisor mode to sepc
+    # Load address of 'user' into t0 and set sepc
+    la      t0, user
+    csrw    sepc, t0
 
+    # Load address of 's_trap' into t1 and set stvec
+    la      t1, s_trap
+    csrw    stvec, t1
+
+    sret                            # Return from S-mode to address in sepc
 
 s_trap:
-    csrr    t0, sepc
-    csrr    t1, scause
-    ecall
+    csrr    t0, sepc               # Read sepc into t0
+    csrr    t1, scause             # Read scause into t1
+    ecall                          # Generate ECALL from S-mode (to M-mode)
 
 user:
-    csrr    t0, instret
-    ecall
+    csrr    t0, instret            # Read 'instret' into t0
+    ecall                          # Generate ECALL from U-mode (to S-mode)
+
